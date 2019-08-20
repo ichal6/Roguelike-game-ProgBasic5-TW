99ad314dc6fa4eff5ff92f4b1c781346bfe9cea9
Michael Lechowicz <ichal.lechowicz@gmail.com>
2019-08-20 10:03:52 +0200

    Add choice == 0 to root_controler

--- controller/root_controller.py
+++ controller/root_controller.py
@@ -32,5 +32,7 @@ def run():
             sales_controller.run()
         elif choice == "6":
             crm_controller.run()
+        elif choice == "0":
+            pass
         else:
             terminal_view.print_error_message("There is no such choice.")
