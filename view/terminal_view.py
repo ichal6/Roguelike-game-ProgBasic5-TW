d254bd88bb63468659b151251253670c666d7ce5
Michael Lechowicz <ichal.lechowicz@gmail.com>
2019-08-20 09:48:21 +0200

    Implementation function get_inputs

--- view/terminal_view.py
+++ view/terminal_view.py
@@ -86,9 +86,13 @@ def get_inputs(list_labels, title):
         list: List of data given by the user. Sample return:
             [<user_input_1>, <user_input_2>, <user_input_3>]
     """
+    # your code
     inputs = []
 
-    # your code
+    print(title)
+    for label in list_labels:
+        data_user = input(label + " ")
+        inputs.append(data_user)
 
     return inputs
 
