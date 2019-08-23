2f27cbc11cca5ad60676156e9479f47d6ae235aa
szczepanTopolski <szczepanPL678@gmail.com>
2019-08-23 11:02:59 +0200

    Clean code controller/common.py

--- controller/common.py
+++ controller/common.py
@@ -4,6 +4,10 @@ implement commonly used functions here
 
 
 def check_is_number(number, list_length):
+    """Check if given input is number and
+    in right partition
+    (number) - input from user
+    (list length) - int, length of list"""
     try:
         number = int(number)
         if number > 0 and number <= list_length:
@@ -15,6 +19,9 @@ def check_is_number(number, list_length):
 
 
 def check_id_by_number(list_of_games, number):
+    """Check record ID from given line number
+    (list of games) - list of id`s
+    (number) - int, line number from user"""
     for list_index in range(len(list_of_games)):
         if number == list_index+1:
             return list_of_games[list_index][0]
